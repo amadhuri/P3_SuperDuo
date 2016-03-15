baf6c4908d64e17ee447b3b7eec7e26c59e35744
amadhuri <athota.madhuri@gmail.com>
2016-03-15 02:27:13 -0700

    Alexandria project added

---
+++ Alexandria/app/src/main/java/it/jaschke/alexandria/BarcodeTrackerFactory.java
@@ -0,0 +1,30 @@
+package it.jaschke.alexandria;
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+import it.jaschke.alexandria.CameraPreview.GraphicOverlay;
+
+/**
+ * Created by Madhuri on 1/19/2016.
+ *
+ *
+ * Factory for creating a tracker and associated graphic to be associated with a new barcode.  The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+
+
+public class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+    private GraphicOverlay<BarcodeGraphic> mGraphicOverlay;
+
+    BarcodeTrackerFactory(GraphicOverlay<BarcodeGraphic> barcodeGraphicOverlay) {
+        mGraphicOverlay = barcodeGraphicOverlay;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        BarcodeGraphic graphic = new BarcodeGraphic(mGraphicOverlay);
+        return new BarcodeGraphicTracker(mGraphicOverlay, graphic);
+    }
+
+}
